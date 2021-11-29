7e472c42d2e20e7408a5d265240003630bf76135
Titan DAC <titandac@titansdac.party>
2021-11-28 16:47:44 -0800

    basic landing page. make better later.

---
+++ titans-dac/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Titans DAC
+        </p>
+        <a
+          className="App-link"
+          href="https://titansdac.party/about"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About
+        </a>
+        <a
+          className="App-link"
+          href="https://portal.metis.io/invite/Wk4s2N4y8e"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join our Mining Pool
+        </a>
+        <a
+          className="App-link"
+          href="https://metacraft.pro/search?query=titans"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Launch Edition NFTs
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
